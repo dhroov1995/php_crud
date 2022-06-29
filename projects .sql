8962bf20c39bf745c62b5fe1e5d376737305f158
Your Name <you@example.com>
2022-06-29 08:45:04 +0530

    all

--- projects .sql
+++ projects .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 25, 2022 at 06:33 AM
+-- Generation Time: Jun 29, 2022 at 05:13 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -43,7 +43,26 @@ CREATE TABLE `tbl_adminmaster` (
 --
 
 INSERT INTO `tbl_adminmaster` (`intAdminID`, `vchEmail`, `vchUserName`, `vchPassword`, `vchName`, `user_type`, `enumStatus`) VALUES
-(1, 'ajay@gmail.com ', 'admin', 'efa545d71e354efbfa6cbe8bbae9ed67', 'demouser', 1, 'A');
+(1, 'ajay@gmail.com ', 'admin', '25d55ad283aa400af464c76d713c07ad', 'demouser', 1, 'A');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_cart`
+--
+
+CREATE TABLE `tbl_cart` (
+  `id` int(11) NOT NULL,
+  `cart_id` int(11) NOT NULL,
+  `product_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_cart`
+--
+
+INSERT INTO `tbl_cart` (`id`, `cart_id`, `product_id`) VALUES
+(1, 10, '0001');
 
 -- --------------------------------------------------------
 
@@ -95,6 +114,27 @@ INSERT INTO `tbl_contact` (`contact_id`, `full_name`, `mobile_no`, `age`, `email
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_order`
+--
+
+CREATE TABLE `tbl_order` (
+  `id` int(11) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `order_by` int(11) NOT NULL,
+  `order_status` enum('success','failed','pending') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_order`
+--
+
+INSERT INTO `tbl_order` (`id`, `product_id`, `order_id`, `order_by`, `order_status`) VALUES
+(1, '0001', 100, 1, 'success');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_product`
 --
 
@@ -113,11 +153,34 @@ CREATE TABLE `tbl_product` (
 --
 
 INSERT INTO `tbl_product` (`id`, `product_image`, `product_category`, `product_code`, `product_name`, `product_colour`, `created_at`) VALUES
-(31, 'images/istockphoto-898449896-1024x1024.jpg', '2', 'sbtv22', 'tv-sm', 'black', '2022-06-21 15:04:56'),
-(32, 'images/download.jpg', '1', '2222', 'bike-h', 'blue', '2022-06-21 15:05:06'),
-(33, 'images/tv.jpg', '2', 'jdsvsv', 'tv-old', 'brown', '2022-06-21 16:26:54'),
-(34, 'images/product-500x500.jpeg', '1', 'new222', 'bike', 'red', '2022-06-22 22:08:40'),
-(35, 'images/tv.jpg', '2', 'ss25', 'tv-sm', 'black', '2022-06-22 22:30:51');
+(1, 'images/istockphoto-898449896-1024x1024.jpg', '2', 'sbtv22', 'tv-sm', 'black', '2022-06-21 15:04:56'),
+(2, 'images/download.jpg', '1', '2222', 'bike-h', 'blue', '2022-06-21 15:05:06'),
+(3, 'images/tv.jpg', '2', 'jdsvsv', 'tv-old', 'brown', '2022-06-21 16:26:54'),
+(4, 'images/product-500x500.jpeg', '1', 'new222', 'bike', 'red', '2022-06-22 22:08:40'),
+(5, 'images/tv.jpg', '2', 'ss25', 'tv-sm', 'black', '2022-06-22 22:30:51');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `name`, `email`, `phone`, `state`, `city`, `address`) VALUES
+(1, 'rohit', 'rohit@gmail.com', '1122334455', 'uttarakhand', 'dehradun', 'dehradun 248001');
 
 --
 -- Indexes for dumped tables
@@ -128,6 +191,12 @@ INSERT INTO `tbl_product` (`id`, `product_image`, `product_category`, `product_c
 --
 ALTER TABLE `tbl_adminmaster`
   ADD PRIMARY KEY (`intAdminID`);
+
+--
+-- Indexes for table `tbl_cart`
+--
+ALTER TABLE `tbl_cart`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbl_category`
@@ -142,9 +211,21 @@ ALTER TABLE `tbl_contact`
   ADD PRIMARY KEY (`contact_id`);
 
 --
+-- Indexes for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tbl_product`
 --
 ALTER TABLE `tbl_product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -156,6 +237,12 @@ ALTER TABLE `tbl_product`
 --
 ALTER TABLE `tbl_adminmaster`
   MODIFY `intAdminID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_cart`
+--
+ALTER TABLE `tbl_cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_category`
@@ -170,10 +257,22 @@ ALTER TABLE `tbl_contact`
   MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `tbl_product`
 --
 ALTER TABLE `tbl_product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
